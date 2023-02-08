ce3cf4a7ce1dc4444defe32be9029e27e31fcff7
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2023-02-07 22:24:05 -0300

    chore: create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PlayerModule } from './player/player.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { TeamModule } from './team/team.module';
 
 @Module({
-  imports: [],
+  imports: [PlayerModule, TeamModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
